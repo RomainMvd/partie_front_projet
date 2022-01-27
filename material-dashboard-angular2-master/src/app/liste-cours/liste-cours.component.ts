fd7ddad5dadcd87e45deb1d2c0ca1346e76e0032
RomainMvd <romain.mvd@outlook.fr>
2022-01-27 17:31:33 +0100

    version debug liste cours

--- material-dashboard-angular2-master/src/app/liste-cours/liste-cours.component.ts
+++ material-dashboard-angular2-master/src/app/liste-cours/liste-cours.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Cours } from 'app/Modeles/cours';
 import { CoursService } from 'app/service/cours.service';
-declare const $: any;
-declare interface RouteInfo {
-  path: string;
-  title: string;
-  icon: string;
-  class: string;
-}
-export const ROUTES: RouteInfo[] = [
-  { path: '/accueil', title: 'Accueil',  icon: 'home', class: '' },
-  { path: '/connexion', title: 'Connexion',  icon:'key', class: '' },
-  { path: '/profil', title: 'Profil',  icon:'person', class: '' },
-  { path: '/vie-sur-le-campus', title: 'Vie sur le campus',  icon:'groups', class: '' },
-  { path: '/engagements-ecologiques', title: 'Engagements écologiques',  icon:'park', class: '' },
-  { path: '/international', title: 'International',  icon:'flight', class: '' },
-  { path: '/contact', title: 'Contact',  icon:'phone', class: '' },
-];
+// declare const $: any;
+// declare interface RouteInfo {
+//   path: string;
+//   title: string;
+//   icon: string;
+//   class: string;
+// }
+// export const ROUTES: RouteInfo[] = [
+//   { path: '/accueil', title: 'Accueil',  icon: 'home', class: '' },
+//   { path: '/connexion', title: 'Connexion',  icon:'key', class: '' },
+//   { path: '/profil', title: 'Profil',  icon:'person', class: '' },
+//   { path: '/vie-sur-le-campus', title: 'Vie sur le campus',  icon:'groups', class: '' },
+//   { path: '/engagements-ecologiques', title: 'Engagements écologiques',  icon:'park', class: '' },
+//   { path: '/international', title: 'International',  icon:'flight', class: '' },
+//   { path: '/contact', title: 'Contact',  icon:'phone', class: '' },
+// ];
 
 
 @Component({
@@ -35,6 +35,6 @@ export class ListeCoursComponent implements OnInit {
   }
   findAll(){
     console.log("Je suis le findAll du composant");
-    this.coursService.findAll().subscribe(data =>{this.coursService = data});
+    this.coursService.findAll().subscribe(data =>{this.courss = data});
   }
 }
