e50bc9cf5f3fe629b7a7ec4f32a217a4ab8c8e08
LaurelineF <laureline.ferrara1@gmail.com>
2022-01-27 16:59:58 +0100

    affichage examen pas fini

--- material-dashboard-angular2-master/src/app/examen/examen.component.ts
+++ material-dashboard-angular2-master/src/app/examen/examen.component.ts
@@ -1,24 +1,30 @@
 import { HttpClient, HttpEventType } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
+import { Examen } from 'app/Modeles/examen';
+import { ExamenService } from 'app/service/examen.service';
 import { Subscription } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'examen',
-  templateUrl: "examen.component.html",
-  styleUrls: ["examen.component.css"]
+  templateUrl: "./examen.component.html",
+  styleUrls: ["./examen.component.css"]
 })
-export class ExamenComponent {
-
-    @Input()
+export class ExamenComponent implements OnInit {
+  [x: string]: any;
+  examens:any;
+  examen : Examen = new Examen();
+  constructor(private examenService: ExamenService) {}
+    
+  @Input()
     requiredFileType:string;
 
     fileName = '';
     uploadProgress:number;
     uploadSub: Subscription;
 
-    constructor(private http: HttpClient) {}
-
+    
+/*
     onFileSelected(event) {
         const file:File = event.target.files[0];
       
@@ -42,7 +48,7 @@ export class ExamenComponent {
             })
         }
     }
-
+*/
   cancelUpload() {
     this.uploadSub.unsubscribe();
     this.reset();
@@ -51,5 +57,12 @@ export class ExamenComponent {
   reset() {
     this.uploadProgress = null;
     this.uploadSub = null;
+  }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+  findAll(){
+    this.examenService.findAll().subscribe(data =>{this.examens = data});
   }
 }
