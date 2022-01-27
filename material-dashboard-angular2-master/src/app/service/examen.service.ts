88584c053d1b9b7ba40639de6598394482d1be20
RomainMvd <romain.mvd@outlook.fr>
2022-01-27 13:40:10 +0100

    perso

--- material-dashboard-angular2-master/src/app/service/examen.service.ts
+++ material-dashboard-angular2-master/src/app/service/examen.service.ts
@@ -20,14 +20,11 @@ export class ExamenService {
 
   public saveExamen(file:File, exam: any): Observable<any>{
     const formData: FormData = new FormData();
-    formData.append('duree', exam.nomPersonne);
-    formData.append('date', exam.prenomPersonne);
+    formData.append('nom exam', exam.nomExamen);
+    formData.append('duree', exam.duree);
+    formData.append('date', exam.dateExamen);
     formData.append('Fichier exam',file);
     formData.append('Fichier reponse',file);
-    formData.append('email', exam.email);
-    formData.append('moyenne', exam.moyenne);
-    formData.append('date de naissance', exam.dateNaissance);
-    formData.append('photo',file);
     const req = new HttpRequest('POST',this.baseUrl,formData, {reportProgress: true, responseType: 'text'});
     return this.httpClient.request(req);
   }
