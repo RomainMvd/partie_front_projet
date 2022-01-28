32b746cfcf1237eb85687aee94b2504903820ca2
in-tl-025 <in-tl-025@localhost.localdomain>
2022-01-28 11:56:58 +0100

    securité

--- material-dashboard-angular2-master/src/app/gestion-utilisateurs/gestion-utilisateurs.component.ts
+++ material-dashboard-angular2-master/src/app/gestion-utilisateurs/gestion-utilisateurs.component.ts
@@ -5,11 +5,11 @@ import { Personne } from 'app/Modeles/personne';
 import { UtilisateurService } from 'app/service/utilisateur.service';
 
 @Component({
-  templateUrl: 'forms.component.html'
+  templateUrl: 'gestion-utilisateurs.component.html'
 })
 export class GestionUtilisateursComponent implements OnInit {
-  Personnes: any;
-  Personne: Personne=new Personne();
+  personnes: any;
+  utilisateur: Personne=new Personne();
   // PersonneExtra: Personne[];
   selectedFiles?: FileList;
   currentFileUpload?: File;
@@ -25,21 +25,21 @@ export class GestionUtilisateursComponent implements OnInit {
     this.findByNomPersonne();
   }*/
   findAll(){
-    this.UtilisateurService.findAll().subscribe(data =>{this.Personnes = data});
+    this.UtilisateurService.findAll().subscribe(data =>{this.personnes = data});
   }
  /* findByNomPersonne(){
     this.UtilisateurService.findByNomPersonne(this.nom).subscribe(data => {this.PersonneExtra = data});
   }*/
-  deletePersonne(id:number){
+  deleteUtilisateur(id:number){
     this.UtilisateurService.delete(id).subscribe( () =>{this.findAll()} );
   }
  selectFile(event:any){
    this.selectedFiles = event.target.files;
  }
- savePersonne(){
+ saveUtilisateur(){
   this.currentFileUpload = this.selectedFiles?.item(0) as File; 
-  this.UtilisateurService.save(this.currentFileUpload, this.Personne).subscribe(
-    () => {this.findAll(); this.Personne = new Personne(); this.selectedFiles = undefined;}
+  this.UtilisateurService.save(this.currentFileUpload, this.utilisateur).subscribe(
+    () => {this.findAll(); this.utilisateur = new Personne(); this.selectedFiles = undefined;}
   )  
  }
  editUser(user:Personne){
